fd14f5a20573ec4db067598846e20cddb7467588
Lmauro97 <112444623+Lmauro97@users.noreply.github.com>
2024-10-09 19:16:03 -0500

    actualizacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   //ng build
+  baseUrl: 'https://backend-e-commerce-website.onrender.com/',
 
-  baseUrl: 'https://backend-e-commerce-website-1-u487.onrender.com/',
+  // baseUrl: 'https://backend-e-commerce-website-1-u487.onrender.com/',
 };
